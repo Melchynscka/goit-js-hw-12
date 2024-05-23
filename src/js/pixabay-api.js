ffefd8b53ced612e853cfb2efa8a1c728a0cada4
Maryna Melchynscka <151916855+Melchynscka@users.noreply.github.com>
2024-05-23 23:04:35 +0300

    Update pixabay-api.js
    px

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 const API_KEY = '43798487-cffd9f7a4f04e7c4b426bb2b8';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 15;
 
-export const fetchPhoto = async (searchImage, page) => {
-  const response = await axios(BASE_URL, {
-    params: {
+export const fetchPhoto = async (query = 'cat', newsPage = 1) => {
+   const params = new URLSearchParams ({
       key: API_KEY,
       q: searchImage,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: 'true',
-      page: page,
-      per_page: 15,
-    },
+      page: newsPage,
   });
-  return response.data;
+ const response = await fetch(`${BASE_URL}/everything?${params}`);
+  const data = await response.json();
+
+  if (data.status === 'error') {
+    throw new Error(data.code);
+  }
+
+  return data;
 };
 
-export default fetchPhoto;
